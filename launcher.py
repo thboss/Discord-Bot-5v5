05cd8a96e25353b945eba5a64578789310e80782
TheBO$$ <40491968+thboss@users.noreply.github.com>
2020-07-25 16:42:44 +0300

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -5,25 +5,35 @@ from bot.bot import LeagueBot
 import asyncio
 import asyncpg
 from dotenv import load_dotenv
-from operator import itemgetter
-from os import environ
+import os
 
+load_dotenv() # Load the environment variables in the local .env file
 
 def run_bot():
     """ Parse the config file and run the bot. """
-    # Load the environment variables in the local .env file
-    load_dotenv()
-
     # Get database object for bot
     connect_url = 'postgresql://{POSTGRESQL_USER}:{POSTGRESQL_PASSWORD}@{POSTGRESQL_HOST}/{POSTGRESQL_DB}'
     loop = asyncio.get_event_loop()
-    db_pool = loop.run_until_complete(asyncpg.create_pool(connect_url.format(**environ)))
+    db_pool = loop.run_until_complete(asyncpg.create_pool(connect_url.format(**os.environ)))
 
+    # Get environment variables
+    bot_token = os.environ['DISCORD_BOT_TOKEN']
+    api_url = os.environ['CSGO_LEAGUE_API_URL']
+    api_key = os.environ['CSGO_LEAGUE_API_KEY']
+    discord_categoty = os.environ['DISCORD_LEAGUE_CATEGORY']
+    discord_role = os.environ['DISCORD_LEAGUE_ROLE']
+    discord_queue = os.environ['DISCORD_LEAGUE_TEXT_QUEUE']
+    discord_commands = os.environ['DISCORD_LEAGUE_TEXT_COMMANDS']
+    discord_results = os.environ['DISCORD_LEAGUE_TEXT_RESULT']
+    discord_lobby = os.environ['DISCORD_LEAGUE_VOICE_LOBBY']
+    discord_language = os.environ['DISCORD_LEAGUE_LANGUAGE']
+
+    if api_url.endswith('/'):
+        api_url = api_url[:-1]
     # Instantiate bot and run
-    env_varnames = ['DISCORD_BOT_TOKEN', 'CSGO_LEAGUE_API_URL', 'CSGO_LEAGUE_API_KEY', 'DISCORD_LEAGUE_CATEGORY',
-                    'DISCORD_LEAGUE_ROLE', 'DISCORD_LEAGUE_TEXT_QUEUE', 'DISCORD_LEAGUE_TEXT_COMMANDS',
-                    'DISCORD_LEAGUE_TEXT_RESULT', 'DISCORD_LEAGUE_VOICE_LOBBY', 'DISCORD_LEAGUE_LANGUAGE']
-    bot = LeagueBot(*itemgetter(*env_varnames)(environ), db_pool=db_pool)
+    bot = LeagueBot(bot_token, api_url, api_key, discord_categoty,
+                    discord_role,discord_queue, discord_commands,
+                    discord_results, discord_lobby, discord_language, db_pool)
     bot.run()
 
 
