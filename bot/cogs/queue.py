0dd0b4ccba7bc27f7b2a39a5b2335ce6a647747f
TheBO$$ <40491968+thboss@users.noreply.github.com>
2020-09-09 15:21:33 +0300

    Update queue.py

--- bot/cogs/queue.py
+++ bot/cogs/queue.py
@@ -14,8 +14,6 @@ class QueueCog(commands.Cog):
         """ Set attributes. """
         self.bot = bot
         self.last_queue_msgs = {}
-        self.before_member_channel = defaultdict(lambda: None)
-        self.after_member_channel = defaultdict(lambda: None)
         self.block_lobby = {}
         self.block_lobby = defaultdict(lambda: False, self.block_lobby)
 
@@ -39,62 +37,59 @@ class QueueCog(commands.Cog):
         embed.set_footer(text=self.bot.translate('receive-notification'))
         return embed
 
-    async def update_last_msg(self, ctx, embed):
+    async def update_last_msg(self, category, embed):
         """ Send embed message and delete the last one sent. """
         try:
-            after_msg = self.last_queue_msgs.get(self.after_member_channel[ctx].category)
+            after_msg = self.last_queue_msgs.get(category)
         except:
             after_msg = None
 
         try:
-            after_text_id = await self.bot.get_league_data(self.after_member_channel[ctx].category, 'text_queue')
+            after_text_id = await self.bot.get_league_data(category, 'text_queue')
         except:
             after_text_id = None
 
-        after_text_channel = ctx.guild.get_channel(after_text_id)
+        after_text_channel = category.guild.get_channel(after_text_id)
 
         if after_msg is None:
-            self.last_queue_msgs[self.after_member_channel[ctx].category] = await after_text_channel.send(
+            self.last_queue_msgs[category] = await after_text_channel.send(
                 embed=embed)
         else:
             try:
                 await after_msg.edit(embed=embed)
             except NotFound:
-                self.last_queue_msgs[self.after_member_channel[ctx].category] = await after_text_channel.send(
+                self.last_queue_msgs[category] = await after_text_channel.send(
                     embed=embed)
 
-    async def _update_last_msg(self, ctx, embed):
+    async def _update_last_msg(self, category, embed):
         """ Send embed message and delete the last one sent. """
 
         try:
-            before_msg = self.last_queue_msgs.get(self.before_member_channel[ctx].category)
+            before_msg = self.last_queue_msgs.get(category)
         except:
             before_msg = None
 
         try:
-            before_text_id = await self.bot.get_league_data(self.before_member_channel[ctx].category, 'text_queue')
+            before_text_id = await self.bot.get_league_data(category, 'text_queue')
         except:
             before_text_id = None
 
-        before_text_channel = ctx.guild.get_channel(before_text_id)
+        before_text_channel = category.guild.get_channel(before_text_id)
 
         if before_msg is None:
-            self.last_queue_msgs[self.before_member_channel[ctx].category] = await before_text_channel.send(
+            self.last_queue_msgs[category] = await before_text_channel.send(
                 embed=embed)
         else:
             try:
                 await before_msg.edit(embed=embed)
             except NotFound:
-                self.last_queue_msgs[self.before_member_channel[ctx].category] = await before_text_channel.send(
+                self.last_queue_msgs[category] = await before_text_channel.send(
                     embed=embed)
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
         if before.channel == after.channel:
             return
-
-        self.before_member_channel[member] = before.channel
-        self.after_member_channel[member] = after.channel
 
         try:
             after_id = await self.bot.get_league_data(after.channel.category, 'voice_lobby')
@@ -109,9 +104,9 @@ class QueueCog(commands.Cog):
         before_lobby = member.guild.get_channel(before_id)
         after_lobby = member.guild.get_channel(after_id)
 
-        match_cog = self.bot.get_cog('MatchCog')
-
         if after.channel == after_lobby is not None:
+            if self.block_lobby[after_lobby.category]:
+                return
 
             if not await self.bot.api_helper.is_linked(member.id):  # Message author isn't linked
                 title = self.bot.translate('account-not-linked').format(member.display_name)
@@ -119,8 +114,8 @@ class QueueCog(commands.Cog):
                 awaitables = [
                     self.bot.api_helper.get_player(member.id),
                     self.bot.db_helper.insert_users(member.id),
-                    self.bot.db_helper.get_queued_users(after.channel.category_id),
-                    self.bot.db_helper.get_league(after.channel.category_id)
+                    self.bot.db_helper.get_queued_users(after_lobby.category_id),
+                    self.bot.db_helper.get_league(after_lobby.category_id)
                 ]
                 results = await asyncio.gather(*awaitables, loop=self.bot.loop)
                 player = results[0]
@@ -136,45 +131,48 @@ class QueueCog(commands.Cog):
                 elif player.in_match:  # member is already in a match
                     title = self.bot.translate('already-in-match').format(member.display_name)
                 else:  # member can be added
-                    await self.bot.db_helper.insert_queued_users(after.channel.category_id, member.id)
+                    await self.bot.db_helper.insert_queued_users(after_lobby.category_id, member.id)
                     queue_ids += [member.id]
                     title = self.bot.translate('added-to-queue').format(member.display_name)
 
                     # Check and burst queue if full
                     if len(queue_ids) == capacity:
-                        self.block_lobby[after.channel.category] = True
-                        pug_role_id = await self.bot.get_league_data(after.channel.category, 'pug_role')
+                        self.block_lobby[after_lobby.category] = True
+                        match_cog = self.bot.get_cog('MatchCog')
+                        pug_role_id = await self.bot.get_league_data(after_lobby.category, 'pug_role')
                         pug_role = member.guild.get_role(pug_role_id)
                         await after_lobby.set_permissions(pug_role, connect=False)
                         queue_members = [member.guild.get_member(member_id) for member_id in queue_ids]
-                        all_readied = await match_cog.start_match(after.channel.category, queue_members)
+                        all_readied = await match_cog.start_match(after_lobby.category, queue_members)
 
                         if all_readied:
-                            await self.bot.db_helper.delete_queued_users(before.channel.category_id, *queue_ids)
-
-                        self.block_lobby[after.channel.category] = False
+                            await self.bot.db_helper.delete_queued_users(after_lobby.category_id, *queue_ids)
+   
+                        self.block_lobby[after_lobby.category] = False
                         await after_lobby.set_permissions(pug_role, connect=True)
                         title = self.bot.translate('players-in-queue')
-                        embed = await self.queue_embed(after.channel.category, title)
-                        await self.update_last_msg(member, embed)
+                        embed = await self.queue_embed(after_lobby.category, title)
+                        await self.update_last_msg(after_lobby.category, embed)
                         return
 
-            embed = await self.queue_embed(after.channel.category, title)
+            embed = await self.queue_embed(after_lobby.category, title)
             # Delete last queue message
-            await self.update_last_msg(member, embed)
+            await self.update_last_msg(after_lobby.category, embed)
 
         if before.channel == before_lobby is not None:
+            if self.block_lobby[before_lobby.category]:
+                return
 
-            removed = await self.bot.db_helper.delete_queued_users(before.channel.category_id, member.id)
+            removed = await self.bot.db_helper.delete_queued_users(before_lobby.category_id, member.id)
 
             if member.id in removed:
                 title = self.bot.translate('removed-from-queue').format(member.display_name)
             else:
                 title = self.bot.translate('not-in-queue').format(member.display_name)
 
-            embed = await self.queue_embed(before.channel.category, title)
+            embed = await self.queue_embed(before_lobby.category, title)
             # Update queue display message
-            await self._update_last_msg(member, embed)
+            await self._update_last_msg(before_lobby.category, embed)
 
     @commands.command(brief='Check if account is linked and give linked role')
     async def check(self, ctx):
@@ -251,7 +249,7 @@ class QueueCog(commands.Cog):
             _embed = self.bot.embed_template(title=title)
             await ctx.send(embed=_embed)
             # Update queue display message
-            await self.update_last_msg(ctx, embed)
+            await self.update_last_msg(ctx.channel.category, embed)
 
     @commands.command(brief='Empty the queue (must have server kick perms)')
     @commands.has_permissions(kick_members=True)
@@ -274,7 +272,7 @@ class QueueCog(commands.Cog):
         _embed = self.bot.embed_template(title=msg)
         await ctx.send(embed=_embed)
         # Update queue display message
-        await self.update_last_msg(ctx, embed)
+        await self.update_last_msg(ctx.channel.category, embed)
 
     @remove.error
     @empty.error
@@ -387,12 +385,6 @@ class QueueCog(commands.Cog):
 
         embed = self.bot.embed_template(title=msg)
         await ctx.send(embed=embed)
-
-    @commands.command()
-    async def delete(self, ctx):
-        for channel in ctx.guild.channels:
-            if channel.name != 'general':
-                await channel.delete()
 
     @lang.error
     @cap.error
