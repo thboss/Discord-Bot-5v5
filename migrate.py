764bf64176a4c4cddcc11a865fd71d124aee6c60
TheBO$$ <40491968+thboss@users.noreply.github.com>
2020-10-16 12:44:07 +0300

    Revert "add default values for psql"
    This reverts commit 49fcf531fae7d9d12618bcc9010489d601a619d4.

--- migrate.py
+++ migrate.py
@@ -9,12 +9,7 @@ from yoyo import get_backend, read_migrations
 def migrate(direction):
     """ Apply Yoyo migrations for a given PostgreSQL database. """
     load_dotenv()
-    user = '{POSTGRESQL_USER}' if '{POSTGRESQL_USER}' else 'csgoleague'
-    password = '{POSTGRESQL_PASSWORD}' if '{POSTGRESQL_PASSWORD}' else 'yourpassword'
-    host = '{POSTGRESQL_HOST}' if '{POSTGRESQL_HOST}' else '127.0.0.1'
-    port = '{POSTGRESQL_PORT}' if '{POSTGRESQL_PORT}' else '5432'
-    db = '{POSTGRESQL_DB}' if '{POSTGRESQL_DB}' else 'csgoleague'
-    connect_url = f'postgresql://{user}:{password}@{host}:{port}/{db}'
+    connect_url = 'postgresql://{POSTGRESQL_USER}:{POSTGRESQL_PASSWORD}@{POSTGRESQL_HOST}/{POSTGRESQL_DB}'
     backend = get_backend(connect_url.format(**environ))
     migrations = read_migrations('./migrations')
     print('Applying migrations:\n' + '\n'.join(migration.id for migration in migrations))
